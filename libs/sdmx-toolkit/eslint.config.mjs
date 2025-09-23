a775d918397d745ce6081b3001f95f29228a2986
Polina <palina_kryvasheyeva@epam.com>
2025-09-23 20:18:09 +0200

    fix: clear dependencies

---
+++ libs/sdmx-toolkit/eslint.config.mjs
@@ -0,0 +1,39 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import js from '@eslint/js';
+import baseConfig from '../../eslint.config.mjs';
+
+const compat = new FlatCompat({
+  baseDirectory: dirname(fileURLToPath(import.meta.url)),
+  recommendedConfig: js.configs.recommended,
+});
+
+export default [
+  {
+    ignores: ['**/dist'],
+  },
+  ...baseConfig,
+
+  {
+    files: ['**/*.json'],
+    rules: {
+      '@nx/dependency-checks': [
+        'error',
+        {
+          ignoredDependencies: [
+            'vite-plugin-svgr',
+            'vite-plugin-static-copy',
+            '@nx/vite',
+            'vite',
+            '@vitejs/plugin-react',
+            'vite-plugin-dts',
+          ],
+        },
+      ],
+    },
+    languageOptions: {
+      parser: await import('jsonc-eslint-parser'),
+    },
+  },
+];
