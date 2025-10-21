542abb0f292d5a184db3b141a4f15af15749df6c
Polina <palina_kryvasheyeva@epam.com>
2025-10-21 15:30:42 +0200

    fix: export interface

--- libs/conversation-view/src/index.ts
+++ libs/conversation-view/src/index.ts
@@ -4,6 +4,8 @@ export {
   AdvancedViewProvider,
   useAdvancedView,
 } from './context/AdvancedViewContext';
+
+export { OnboardingProvider } from './context/OnboardingContext';
 export * from './models/actions';
 export * from './models/titles';
 export * from './models/attachments-styles';
@@ -14,3 +16,6 @@ export { AdvancedView } from './components/AdvancedView/AdvancedView';
 export { ConversationView } from './components/ConversationView/ConversationView';
 // eslint-disable-next-line @nx/enforce-module-boundaries
 export type { ShareConversationProps } from '../../share-conversation/src/models/share-conversation';
+
+// eslint-disable-next-line @nx/enforce-module-boundaries
+export type { UserInfo } from '../../user-info/src/models/user-info';
