9e59a685cbef3e05b664cbc5cd4f566718983a49
Palina_Kryvasheyeva <Palina_Kryvasheyeva@epam.com>
2025-10-21 15:42:55 +0200

    fix: export interface (#22)

--- libs/conversation-view/src/index.ts
+++ libs/conversation-view/src/index.ts
@@ -4,13 +4,19 @@ export {
   AdvancedViewProvider,
   useAdvancedView,
 } from './context/AdvancedViewContext';
+
+export { OnboardingProvider } from './context/OnboardingContext';
 export * from './models/actions';
 export * from './models/titles';
 export * from './models/attachments-styles';
 export * from './models/structure-component';
+export * from './models/message';
 export * from './types/charting-icon';
 export { ConversationWelcome } from './components/ConversationWelcome/ConversationWelcome';
 export { AdvancedView } from './components/AdvancedView/AdvancedView';
 export { ConversationView } from './components/ConversationView/ConversationView';
 // eslint-disable-next-line @nx/enforce-module-boundaries
 export type { ShareConversationProps } from '../../share-conversation/src/models/share-conversation';
+
+// eslint-disable-next-line @nx/enforce-module-boundaries
+export type { UserInfo } from '../../user-info/src/models/user-info';
--- libs/dial-toolkit/src/index.ts
+++ libs/dial-toolkit/src/index.ts
@@ -1,4 +1,5 @@
 export * from './constants/api-urls';
+export * from './constants/share-conversation';
 export * from './models';
 export * from './types';
 export * from './utils';
