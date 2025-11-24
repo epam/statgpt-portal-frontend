9a9c58f62771ad5d23c1995334f8d1956773c944
Palina_Kryvasheyeva <Palina_Kryvasheyeva@epam.com>
2025-11-24 11:27:27 +0100

    chore: hide apim header (#40)

--- libs/shared-toolkit/src/constants/headers.ts
+++ libs/shared-toolkit/src/constants/headers.ts
@@ -2,3 +2,4 @@ export const APPLICATION_JSON = 'application/json';
 export const CONTENT_TYPE_HEADER = 'Content-Type';
 export const API_KEY_HEADER = 'Api-Key';
 export const X_CONVERSATION_ID_HEADER = 'X-CONVERSATION-ID';
+export const OCP_APIM_SUBSCRIPTION_KEY_HEADER = 'Ocp-Apim-Subscription-Key';
