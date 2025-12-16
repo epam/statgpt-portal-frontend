043bb8ab825fd590387fccc0b51f1931c104e17a
MargoGrodno-epam <Marharyta_Buikevich@epam.com>
2025-12-16 16:21:58 +0100

    chore: update exports (#68)

--- libs/conversation-list/src/index.ts
+++ libs/conversation-list/src/index.ts
@@ -6,4 +6,9 @@ export * from './utils/shared-conversations';
 export { ConversationList } from './components/ConversationList/ConversationList';
 export { ActionMenu } from './components/ActionMenu/ActionMenu';
 // eslint-disable-next-line @nx/enforce-module-boundaries
-export * from '../../user-info/src';
+export type {
+  UserInfo,
+  SignOutTitles,
+} from './../../user-info/src/models/user-info';
+// eslint-disable-next-line @nx/enforce-module-boundaries
+export { User } from './../../user-info/src/components/User/User';
