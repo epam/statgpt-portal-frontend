2576cadf8e05a3ece1d285a1c22b572b3501c8de
Marharyta Buikevich <marharyta_buikevich@epam.com>
2026-03-05 10:28:40 +0100

    chore: correct jest configs

--- libs/shared-toolkit/jest.config.js
+++ libs/shared-toolkit/jest.config.js
@@ -6,6 +6,5 @@ module.exports = {
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/libs/shared-toolkit',
-  setupFilesAfterEnv: ['./test-setup.js'],
+  coverageDirectory: '../../coverage/libs/shared-toolkit'
 };
--- libs/ui-components/jest.config.js
+++ libs/ui-components/jest.config.js
@@ -7,5 +7,5 @@ module.exports = {
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/libs/ui-components',
-  setupFilesAfterEnv: ['./test-setup.js', './jest-env-mocks.ts'],
+  setupFilesAfterEnv: ['./jest-env-mocks.ts'],
 };
