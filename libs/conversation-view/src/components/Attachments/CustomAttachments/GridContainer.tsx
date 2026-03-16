1dc7a8e47b9a41177fa0baf5fd540eefc1331e74
Marharyta Buikevich <marharyta_buikevich@epam.com>
2026-03-16 16:12:24 +0100

    chore: regular ref instead of forwardRef

--- libs/conversation-view/src/components/Attachments/CustomAttachments/GridContainer.tsx
+++ libs/conversation-view/src/components/Attachments/CustomAttachments/GridContainer.tsx
@@ -1,29 +1,28 @@
 'use client';
 
-import { forwardRef, ReactNode } from 'react';
+import { FC, Ref, ReactNode } from 'react';
 import classNames from 'classnames';
 
 interface Props {
+  ref?: Ref<HTMLDivElement>;
   fixHeight?: boolean;
   gridHeight: number;
   children: ReactNode;
 }
 
-const GridContainer = forwardRef<HTMLDivElement, Props>(
-  ({ fixHeight, gridHeight, children }, ref) => {
-    return (
-      <div
-        ref={ref}
-        className={classNames(
-          'ag-theme-quartz w-full min-h-[80px]',
-          fixHeight ? 'max-h-[400px]' : 'max-h-full',
-        )}
-        style={{ height: gridHeight }}
-      >
-        {children}
-      </div>
-    );
-  },
-);
+const GridContainer: FC<Props> = ({ ref, fixHeight, gridHeight, children }) => {
+  return (
+    <div
+      ref={ref}
+      className={classNames(
+        'ag-theme-quartz w-full min-h-[80px]',
+        fixHeight ? 'max-h-[400px]' : 'max-h-full',
+      )}
+      style={{ height: gridHeight }}
+    >
+      {children}
+    </div>
+  );
+};
 
 export default GridContainer;
