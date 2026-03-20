0d874c9e21be2a70199cf780820f852110b887c7
Marharyta Buikevich <marharyta_buikevich@epam.com>
2026-03-20 01:09:53 +0100

    chore: fix requests & package-lock.json update

--- libs/conversation-view/src/utils/attachments/attachments-data.ts
+++ libs/conversation-view/src/utils/attachments/attachments-data.ts
@@ -53,8 +53,9 @@ export const getStructureDataMaps = async (
 ): Promise<StructureDataMaps> => {
   const structureDataMaps = initStructureDataMaps();
 
-  for (const dataQuery of dataQueries) {
-    getDataSetAction(dataQuery.urn).then(async (dataSet) => {
+  await Promise.all(
+    dataQueries.map(async (dataQuery) => {
+      const dataSet = await getDataSetAction(dataQuery.urn);
       if (dataSet?.data) {
         const dimensions = getDimensions(dataSet.data);
 
@@ -78,7 +79,7 @@ export const getStructureDataMaps = async (
           dimensions,
         );
 
-        getDataSetData(
+        await getDataSetData(
           dataQuery,
           { filterKey, timeFilter },
           getDataSetDataAction,
@@ -92,8 +93,8 @@ export const getStructureDataMaps = async (
           })
           .finally(() => setIsLoadingGridData(false));
       }
-    });
-  }
+    }),
+  );
   return structureDataMaps;
 };
 
