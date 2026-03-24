324820c0340150ef1d4004f73285556479f85189
Mikhail Hahalushka <Mikhail_Hahalushka@epam.com>
2026-03-24 12:32:36 +0400

    border top only when is not cross dataset

--- libs/conversation-view/src/components/AdvancedView/AdvancedView.tsx
+++ libs/conversation-view/src/components/AdvancedView/AdvancedView.tsx
@@ -217,7 +217,12 @@ const AdvancedViewInternal: FC<Props> = ({
                   externalLink={externalLink}
                 />
               )}
-              <div className="flex flex-1 min-h-0 overflow-auto border-t border-neutrals-500">
+              <div
+                className={classNames([
+                  'flex flex-1 min-h-0 overflow-auto',
+                  !isCrossDatasetModeOn && 'border-t border-neutrals-500',
+                ])}
+              >
                 <div
                   className={classNames(
                     'flex-1 min-h-0 overflow-auto',
