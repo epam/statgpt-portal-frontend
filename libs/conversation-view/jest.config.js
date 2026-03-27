86b24d3116bcd1de69791bbddc3142b0ae50919d
Andrii Stropalov <andrii_stropalov@epam.com>
2026-03-27 12:05:20 +0100

    Fix test runner for all libs

--- libs/conversation-view/jest.config.js
+++ libs/conversation-view/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  displayName: 'conversation-list',
+  displayName: 'conversation-view',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/libs/conversation-list',
+  coverageDirectory: '../../coverage/libs/conversation-view',
 };
