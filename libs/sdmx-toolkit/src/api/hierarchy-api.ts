b283f06475e1f7fa391a998a8f061f35d06bb0a5
Andrii Stropalov <andrii_stropalov@epam.com>
2026-04-10 18:46:37 +0200

    Add tokens to a new routes and api

--- libs/sdmx-toolkit/src/api/hierarchy-api.ts
+++ libs/sdmx-toolkit/src/api/hierarchy-api.ts
@@ -7,6 +7,7 @@ export class HierarchyApi {
 
   async getAvailableHierarchies(
     codelistUrn: string,
+    token?: string,
   ): Promise<StructuralMetaData> {
     const { agency, id, version } = splitUrn(codelistUrn);
     const baseUrl = `${this.client.config.sdmxProxyUrl}/sdmx/3.0/structure/codelist`;
@@ -14,16 +15,21 @@ export class HierarchyApi {
       `${agency}/${id}/${version}?references=hierarchy&detail=allcompletestubs`,
       { method: 'GET' },
       baseUrl,
+      token,
     );
   }
 
-  async getHierarchy(hierarchyUrn: string): Promise<StructuralMetaData> {
+  async getHierarchy(
+    hierarchyUrn: string,
+    token?: string,
+  ): Promise<StructuralMetaData> {
     const { agency, id, version } = splitUrn(hierarchyUrn);
     const baseUrl = `${this.client.config.sdmxProxyUrl}/sdmx/3.0/structure/hierarchy`;
     return this.client.request<StructuralMetaData>(
       `${agency}/${id}/${version}?references=descendants&detail=full`,
       { method: 'GET' },
       baseUrl,
+      token,
     );
   }
 }
