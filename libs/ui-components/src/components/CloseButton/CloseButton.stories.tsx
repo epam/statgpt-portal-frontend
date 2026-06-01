2a355e27185dc3fbdf484e8202d3b5b712b80aaa
Mikhail Hahalushka <Mikhail_Hahalushka@epam.com>
2026-06-01 19:50:37 +0400

    feat: add CloseButton story

---
+++ libs/ui-components/src/components/CloseButton/CloseButton.stories.tsx
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { CloseButton } from './CloseButton';
+
+const meta: Meta<typeof CloseButton> = {
+  title: 'UI Components/CloseButton',
+  component: CloseButton,
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof CloseButton>;
+
+export const Default: Story = {};
+
+export const WithTitle: Story = {
+  name: 'With Title',
+  args: {
+    title: 'Close',
+  },
+};
+
+export const LargeIcon: Story = {
+  name: 'Large Icon',
+  args: {
+    iconWidth: 32,
+    iconHeight: 32,
+  },
+};
+
+export const SmallIcon: Story = {
+  name: 'Small Icon',
+  args: {
+    iconWidth: 12,
+    iconHeight: 12,
+  },
+};
+
+export const WithCustomClass: Story = {
+  name: 'With Custom Class',
+  args: {
+    btnClassNames: 'rounded-full bg-neutrals-300 p-1 hover:bg-neutrals-400',
+  },
+};
