32e4be9a2cabcd6e1b8564f71d8291b6ccc1a37d
Mikhail Hahalushka <Mikhail_Hahalushka@epam.com>
2026-06-01 19:17:16 +0400

    feat: add Alert story with transform-based fixed positioning fix

--- apps/storybook/.storybook/brand-configs/brand2/index.tsx
+++ apps/storybook/.storybook/brand-configs/brand2/index.tsx
@@ -6,9 +6,9 @@ import InfoIcon from '../assets/info.svg';
 
 export const brand2AlertConfig: InlineAlertConfig = {
   icons: {
-    error: <ErrorIcon className="text-semantic-error size-6" />,
-    warning: <WarningIcon className="text-semantic-warning size-6" />,
-    info: <InfoIcon className="text-neutrals-800 size-6" />,
+    error: <ErrorIcon className="size-6 text-semantic-error" />,
+    warning: <WarningIcon className="size-6 text-semantic-warning" />,
+    info: <InfoIcon className="size-6 text-neutrals-800" />,
   },
   classes: {
     container: 'flex gap-2 min-w-0 border rounded py-2 px-6 items-center',
