967dc6c70552b2bc942b6f84b36bb58d1c391632
Andrii Stropalov <andrii_stropalov@epam.com>
2026-06-22 10:00:41 +0200

    Host fonts in the app.

--- apps/portals-example/src/app/layout.tsx
+++ apps/portals-example/src/app/layout.tsx
@@ -1,13 +1,20 @@
 import './globals.scss';
-import { Inter, Open_Sans } from 'next/font/google';
+import { Inter } from 'next/font/google';
+import localFont from 'next/font/local';
 import classNames from 'classnames';
 import { ReactNode } from 'react';
 import SessionProviderWrapper from '../components/SessionProvider';
 import { auth } from '../auth';
 
-const font = Open_Sans({
-  subsets: ['latin'],
-  weight: 'variable',
+const font = localFont({
+  src: [
+    { path: '../fonts/open-sans-400.woff2', weight: '400', style: 'normal' },
+    { path: '../fonts/open-sans-600.woff2', weight: '600', style: 'normal' },
+    { path: '../fonts/open-sans-700.woff2', weight: '700', style: 'normal' },
+  ],
+  display: 'swap',
+  fallback: ['Arial', 'sans-serif'],
+  adjustFontFallback: false,
   variable: '--font-open-sans',
 });
 
@@ -35,7 +42,9 @@ export default async function RootLayout({
         <link rel="icon" href={'/images/favicon.ico'} sizes="any" />
         <link rel="apple-touch-icon" href={'/images/favicon.ico'} />
       </head>
-      <body className={classNames(font.variable, inter.variable)}>
+      <body
+        className={classNames(font.variable, inter.variable, 'antialiased')}
+      >
         <SessionProviderWrapper session={session}>
           {children}
         </SessionProviderWrapper>
